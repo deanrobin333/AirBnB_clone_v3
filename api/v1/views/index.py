fcfb8c1beb434004cc19bb602118637d4ed7f176
deanrobin333 <deanrobin777@gmail.com>
2024-08-06 05:16:26 +0300

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,28 @@
 '''api status'''
 from flask import jsonify
 from api.v1.views import app_views
+import models
+from models import storage
+from models.base_model import BaseModel
 
 
-@app_views.route('/status', strict_slashes=False)
+app.url_map.strict_slashes = False
+
+
+@app_views.route('/status')
 def returnstuff():
     '''return stuff'''
     return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def stuff():
+    '''JSON Responses'''
+    todos = {
+            'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review
+            }
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
