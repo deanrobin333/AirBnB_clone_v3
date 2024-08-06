ee876c53efbda8b25dc6d6db7ae122d323edc4dc
deanrobin333 <deanrobin777@gmail.com>
2024-08-06 04:33:36 +0300

    api/__init__.py, api/v1/__init__.py, api/v1/views/__init__.py, api/v1/views/index.py, api/v1/app.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# index.py
+
+'''api status'''
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
