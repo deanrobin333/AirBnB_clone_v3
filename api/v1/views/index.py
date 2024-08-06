e675bb357966deab8575780ae47da778ab59d996
deanrobin333 <deanrobin777@gmail.com>
2024-08-06 05:25:24 +0300

    updated index.py and modified import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,9 @@
 # index.py
 
 '''api status'''
-from flask import jsonify
-from api.v1.views import app_views
-import models
 from models import storage
-from models.base_model import BaseModel
+from flask import Flask, jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,15 +12,14 @@ def returnstuff():
     '''return stuff'''
     return jsonify(status='OK')
 
-
 @app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {
-            'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review
-            }
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
